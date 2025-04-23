35a5124338cd29a4e138b711703decdc61e8ba3d
anjanisone <emailing2prasanna@gmail.com>
2025-04-23 21:25:19 +0400

    Update etl_new.py

--- etl_new.py
+++ etl_new.py
@@ -1,80 +1,98 @@
-from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import col, expr, to_date, date_format, date_add
-from pyspark.sql.window import Window
-
-from Attributes import Attributes
-from taslibrary import sns_helper as sns_service
-from taslibrary import glue_service, s3_service, logService
-from utils import Utils
 import sys
 
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.window import Window
+from pyspark.sql.functions import *
 
-def load_allocations(spark: SparkSession, glue_helper, s3_helper, args: dict) -> DataFrame:
-    df = glue_helper.read_sql_to_df(
+from attributes import Attributes
+from taslibrary import sns_helper as sns_service
+from taslibrary import glue_service, s3_service, log_service
+from utils import Utils
+from taslibrary.ingestion_exceptions.manager import InvalidProcessTypeException
+from taslibrary.tas_onetick_query_helper import TasOneTickQueryHelper
+import taslibrary.tas_onetick_utility as tas_onetick_util
+
+
+
+otq_helper = TasOneTickQueryHelper(sys_level = "eng", sys_type="glue")
+
+def extract_allocations(spark: SparkSession, glue_helper, s3_helper, args: dict):
+    logger.info("Loading and deduplicating allocation data")
+    alloc_df = glue_helper.read_sql_to_df(
         spark,
         args["awsApplicationPayloadS3Bucket"],
-        Attributes.SQL_SCRIPTS_PATH.value + Attributes.ALLOCATIONS_QUERY_PATH.value,
+        Attributes.SQL_SCRIPTS_PATH.value + Attributes.ALLOCATIONS_QUERY_PATH.value
     )
 
-    df = df.filter(
-        (col("sec_typ_cd").isin("ADR", "COM", "GDR", "MFC", "MFO", "PFD")) &
-        (col("status") == "ACCT")
+    logger.info(f"Fetched allocation records: {alloc_df.count()}")
+    s3_helper.upload_process_logs_spdf(
+        alloc_df,
+        args["s3TCASecIdBucket"].replace("s3://", ""),
+        args["JOB_NAME"],
+        "raw_allocations"
     )
 
-    df = df.select(
-        "sec_id",
-        to_date(col("crdtradedate").cast("string").substr(1, 10)).alias("crdtradedate"),
-        to_date(col("trade_date_local").cast("string").substr(1, 10)).alias("trade_date_local"),
-        to_date(col("trade_date_est").cast("string").substr(1, 10)).alias("trade_date_est"),
-        to_date(col("trade_date_utc").cast("string").substr(1, 10)).alias("trade_date_utc")
+    window = Window.partitionBy("sec_id", "trade_date_est").orderBy("trade_date_est")
+    alloc_dedup_df = (
+        alloc_df.withColumn("row_number", row_number().over(window))
+        .filter(col("row_number") == 1)
+        .drop("row_number")
     )
 
-    df = df.dropDuplicates(["sec_id", "trade_date_est"])
+    s3_helper.upload_process_logs_spdf(
+        alloc_dedup_df,
+        args["s3TCASecIdBucket"].replace("s3://", ""),
+        args["JOB_NAME"],
+        "dedup_allocations"
+    )
 
-    logger.info(f"Allocations cleaned and deduplicated: {df.count()} records")
-    s3_helper.upload_process_logs_spdf(df.limit(1000), args["s3TCASecIdBucket"].replace("s3://", ""), args["JOB_NAME"], "allocations_cleaned")
-    return df
+    return alloc_dedup_df
 
 
-def load_csm_security(spark: SparkSession, s3_helper, args: dict) -> DataFrame:
-    logger.info("Loading CSM security data")
+def extract_securities(spark: SparkSession, s3_helper, args: dict) -> DataFrame:
+    logger.info("Loading CRD security data")
 
-    df = glue_helper.read_sql_to_df(
+    securities_df = glue_helper.read_sql_to_df(
         spark,
         args["awsApplicationPayloadS3Bucket"],
-        Attributes.SQL_SCRIPTS_PATH.value + Attributes.CSM_SECURITY_UNION_QUERY_PATH.value,
-    ).withColumn("source", expr("'O'"))
-
-    df_clean = df.select(
-        to_date(col("datadate").cast("string"), "yyyyMMdd").alias("datadate"),
-        "sec_id", "ext_sec_id", "sedol", "cusip", "isin_no", "ticker"
+        Attributes.SQL_SCRIPTS_PATH.value + Attributes.CSM_SECURITY_UNION_QUERY_PATH.value
     )
 
-    logger.info(f"CSM security cleaned: {df_clean.count()} records")
-    s3_helper.upload_process_logs_spdf(df_clean.limit(1000), args["s3TCASecIdBucket"].replace("s3://", ""), args["JOB_NAME"], "csm_cleaned")
+    logger.info(f"CRD security historic: {securities_df.count()} records")
+    s3_helper.upload_process_logs_spdf(
+        securities_df.limit(10000),
+        args["s3TCASecIdBucket"].replace("s3://", ""),
+        args["JOB_NAME"],
+        "historic_crd_securities"
+    )
 
-    return df_clean
+    return securities_df
 
 
-def join_allocations_with_security(spark, alloc_df, sec_df, s3_helper, args):
-    logger.info("Joining allocations with cleaned CSM security using Spark SQL")
+def join_orders_with_security(allocations_df: DataFrame, securities_df: DataFrame, s3_helper, args: dict) -> DataFrame:
+    logger.info("Joining allocations with CRD securities using Spark SQL")
 
-    alloc_df_casted = alloc_df.withColumn("sec_id_str", col("sec_id").cast("string"))
-    alloc_df_casted.createOrReplaceTempView("alloc_df")
-    sec_df.createOrReplaceTempView("csm_df")
+    allocations_df_casted = allocations_df.withColumn("sec_id_str", col("sec_id").cast("string"))
+    allocations_df_casted.createOrReplaceTempView("allocations_tmp")
+    securities_df.createOrReplaceTempView("crd_securities_tmp")
 
-    sql_path = Attributes.SQL_SCRIPTS_PATH.value + Attributes.ALLOC_CSM_JOIN_SQL.value
-    sql_query = Utils(logger).read_sql_file(sql_path)
+    allocations_securities_join_df = glue_helper.read_sql_to_df(
+        spark,
+        args["awsApplicationPayloadS3Bucket"],
+        Attributes.SQL_SCRIPTS_PATH.value + Attributes.ALLOC_SECURITY_JOIN_SQL.value
+    )
 
-    joined_df = spark.sql(sql_query)
+    logger.info(f"Total records after allocations + CRD join: {allocations_securities_join_df.count()}")
+    s3_helper.upload_process_logs_spdf(
+        allocations_securities_join_df.limit(10000),
+        args["s3TCASecIdBucket"].replace("s3://", ""),
+        args["JOB_NAME"],
+        "historic_allocations_securities"
+    )
 
-    logger.info(f"Total records after allocations + CSM join: {joined_df.count()}")
-
-    null_df = joined_df.filter(
-        col("ext_sec_id").isNull() &
-        col("sedol").isNull() &
-        col("cusip").isNull() &
-        col("isin_no").isNull() &
+    null_df = allocations_securities_join_df.filter(
+        col("ext_sec_id").isNull() |
+        col("sedol").isNull() |
         col("ticker").isNull()
     )
 
@@ -82,105 +100,112 @@ def join_allocations_with_security(spark, alloc_df, sec_df, s3_helper, args):
         null_df.limit(1000),
         args["s3TCASecIdBucket"].replace("s3://", ""),
         args["JOB_NAME"],
-        "allocations_security_extsecid_null"
+        "historic_allocations_securities_null"
     )
 
-    return joined_df
+    return allocations_securities_join_df
 
 
-def load_and_join_srm(spark, glue_helper, s3_helper, joined_df: DataFrame, args: dict) -> DataFrame:
-    logger.info("Joining with SRM and SRMMF using string-based joins on ext_sec_id and previous_trade_date_est")
+def extract_and_join_orders_srm(spark, glue_helper, s3_helper, allocations_securities_df: DataFrame, args: dict) -> DataFrame:
+    logger.info("Joining with SRM_EQUITY and SRM_MF using Spark SQL from SQL file")
 
-    joined_df = joined_df.withColumn("previous_trade_date_est", date_add(col("trade_date_est"), -1))
-    joined_df = joined_df.withColumn("previous_trade_date_est_str", date_format(col("previous_trade_date_est"), "yyyyMMdd"))
-    joined_df.createOrReplaceTempView("alloc_csm_view")
+    allocations_securities_df = allocations_securities_df.withColumn(
+        "previous_trade_date_est", date_add(col("trade_date_est"), -1)
+    )
 
-    srm_df = glue_helper.read_sql_to_df(
+    allocations_securities_df = allocations_securities_df.withColumn(
+        "previous_trade_date_est_str", date_format(col("previous_trade_date_est"), "yyyyMMdd")
+    )
+
+    allocations_securities_df.createOrReplaceTempView("alloc_securities_tmp")
+
+    alloc_securities_srm_eq_mf_df = glue_helper.read_sql_to_df(
         spark,
         args["awsApplicationPayloadS3Bucket"],
-        Attributes.SQL_SCRIPTS_PATH.value + Attributes.SRM_QUERY_PATH.value
+        Attributes.SQL_SCRIPTS_PATH.value + Attributes.ALLOC_SECURITY_SRM_JOIN_SQL.value
     )
-    srm_df.createOrReplaceTempView("srm")
 
-    srmmf_df = glue_helper.read_sql_to_df(
-        spark,
-        args["awsApplicationPayloadS3Bucket"],
-        Attributes.SQL_SCRIPTS_PATH.value + Attributes.SRMMF_QUERY_PATH.value
-    )
-    srmmf_df.createOrReplaceTempView("srmmf")
-
-    sql_path = Attributes.SQL_SCRIPTS_PATH.value + Attributes.ALLOC_SRM_SRMMF_JOIN_SQL.value
-    sql_query = Utils(logger).read_sql_file(sql_path)
-
-    final_df = spark.sql(sql_query)
-
-    logger.info(f"Final SRM+SRMMF join result count: {final_df.count()}")
-
-    # ------------------- FINAL DATAFRAME ASSERTIONS -------------------
-
-    def get_ticker_map(df, id_col, id_val, date_col, date_vals):
-        return {row[date_col]: row["ticker"] for row in df.filter((col(id_col) == id_val) & col(date_col).isin(date_vals)).select(date_col, "ticker").collect()}
-
-    assert_map = [
-        ("80931510", "trade_date_est", ["2024-11-14", "2024-11-15", "2024-11-16", "2024-11-17", "2024-11-18", "2024-11-19", "2024-11-20"], "SVW", "SGH", "2024-11-18"),
-        ("3817154325", "trade_date_est", ["2025-02-20", "2025-02-21", "2025-02-22", "2025-02-23", "2025-02-24", "2025-02-25"], "QRTEB", "QVCGB", "2025-02-24"),
-        ("3259235802", "trade_date_est", ["2025-02-26", "2025-02-27", "2025-02-28"], "MPLN", "CTEV", "2025-02-28")
-    ]
-
-    for sec_id, date_col, dates, before_val, after_val, switch_date in assert_map:
-        sub_df = final_df.filter(col("sec_id") == sec_id).select(date_col, "ticker")
-        date_map = {row[date_col].strftime("%Y-%m-%d"): row["ticker"] for row in sub_df.filter(col(date_col).isin(dates)).collect()}
-        logger.info(f"[FinalDF Assertion] sec_id={sec_id} ticker by {date_col}: {date_map}")
-        for d in dates:
-            if d < switch_date:
-                assert date_map[d] == before_val, f"Expected {before_val} before {switch_date} for {sec_id}, got {date_map[d]} on {d}"
-            else:
-                assert date_map[d] == after_val, f"Expected {after_val} from {switch_date} for {sec_id}, got {date_map[d]} on {d}"
+    logger.info(f"Final SRM_EQUITY+SRM_MF join result count: {alloc_securities_srm_eq_mf_df.count()}")
 
     s3_helper.upload_process_logs_spdf(
-        final_df.limit(1000),
+        alloc_securities_srm_eq_mf_df.limit(10000),
         args["s3TCASecIdBucket"].replace("s3://", ""),
         args["JOB_NAME"],
-        "final_output_neoxam_srm_srmmf"
+        "historic_allocations_srm_join"
     )
 
-    return final_df
+        # ------------------- Write to Hive/Glue Table -------------------
+    output_table = Attributes.SECURITY_IDENTIFIER_OUTPUT_TABLE.value  # Define this in Attributes.py
+    logger.info(f"Writing final output to table: {output_table}")
+
+    alloc_securities_srm_eq_mf_df.write.mode("overwrite").saveAsTable(output_table)
+
+    return alloc_securities_srm_eq_mf_df
+
+
+def do_otq(otq_name, graph_name, query_params):
+    logger.info(f"Running OTQ query: {otq_name} with params: {query_params}")
+    otq_test_response = otq_helper.run(f'{otq_name}::{graph_name}', query_params, output_structure = "OutputStructure.symbol_result_list")
+    logger.info("OTQ query completed successfully.")
+
+    return tas_onetick_util.otq_from_list_to_df(otq_test_response)
 
 
 if __name__ == "__main__":
-    logger = logService.get_logger()
-    logger.info("Starting security_trade_date_identifiers ETL job")
+    logger = log_service.LogService(
+        app_prefix=Attributes.APP_PREFIX.value,
+        job_name=Attributes.GLUE_JOB_NAME.value
+    ).get_logger()
+
     util = Utils(logger)
-    args = util.get_args_dict(sys_args_list=Attributes.SVS_ARGS_LIST.value)
+    args = util.get_args_dict(sys_args_list=Attributes.SYS_ARGS_LIST.value)
 
     spark = util.get_spark_session()
     glue_helper = glue_service.GlueService(logger)
     s3_helper = s3_service.S3Service(logger, args["sysLevel"])
+    sns_helper = sns_service.SnsHelper(args["sysLevel"])
 
     logger.info(f"Python Version: {sys.version}")
-    logger.info(f"Spark Version: {spark.version}")
+    logger.info(f"PySpark Version: {spark.version}")
     logger.info(f"Job Arguments: {args}")
 
     logger.info("Glue Job Initialized")
-    logger.info(f"Process type: {args['processType']}")
+    logger.info(f"Starting security trade date identifiers ETL job. Process type: {args['processType']}")
 
     if args['processType'] == "daily":
         pass
 
     elif args['processType'] == "historic":
-        allocations_df = load_allocations(spark, glue_helper, s3_helper, args)
-        logger.info(f"Allocations loaded: {allocations_df.count()} records")
+        allocations_df = extract_allocations(spark, glue_helper, s3_helper, args)
+        securities_df = extract_securities(spark, s3_helper, args)
+        allocations_securities_df = join_orders_with_security(allocations_df, securities_df, s3_helper, args)
+        alloc_securities_srm_srmuf = extract_and_join_orders_srm(
+            spark, glue_helper, s3_helper, allocations_securities_df, args
+        )
+        logger.info("Security trade date identifiers job completed successfully.")
 
-        csm_df = load_csm_security(spark, s3_helper, args)
-        logger.info(f"CSM security loaded: {csm_df.count()} records")
-
-        joined_df = join_allocations_with_security(spark, allocations_df, csm_df, s3_helper, args)
-        logger.info(f"Joined allocations with CSM security: {joined_df.count()} records")
-
-        final_df = load_and_join_srm(spark, glue_helper, s3_helper, joined_df, args)
-        final_df.show(truncate=False)
-
-        logger.info("ETL job completed successfully.")
-
+    else:
+        logger.error("Process Type not in (historic, daily) - Invalid Process Type")
+        raise InvalidProcessTypeException(args['processType'])
+    
+    otq_file_name = Attributes.OTQ_FILE_NAME.value
+    graph_name = Attributes.OTQ_GRAPH_NAME.value
+    query_params = {
+    "start_date": args["startDate"],
+    "end_date": args["endDate"],
+    "sec_id_list": args["secIdList"],
+    "giveFile": args["giveFile"],
+    "prior_days": 5,
+    "after_days": 10
+    }
+    otq_df = do_otq(otq_file_name, graph_name, query_params)
+    logger.info(f"OTQ DataFrame: {otq_df.show(5)}")
+    # ------------------- Write to S3 -------------------
+    s3_helper.upload_process_logs_spdf(
+        otq_df,
+        args["s3TCASecIdBucket"].replace("s3://", ""),
+        args["JOB_NAME"],
+        "otq_results"
+    )
     spark.stop()
     logger.info("Spark session stopped.")
